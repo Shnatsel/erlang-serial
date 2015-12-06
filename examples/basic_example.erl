6198356b49348499d1a738397c86c92e6660dc53
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2015-12-06 05:12:19 +0300

    make basic_example.erl actually useful for our use case

--- examples/basic_example.erl
+++ examples/basic_example.erl
@@ -3,7 +3,7 @@
 -export([open/0, close/1, send/1, listen/0]).
 
 open() ->
-  SerialPort = serial:start([{open, "/dev/ttyUSB0"}, {speed, 9600}]),
+  SerialPort = serial:start([{open, "/dev/ttyAMA0"}, {speed, 9600}]),
   {ok, SerialPort}.
 
 close(SerialPort) ->
@@ -11,14 +11,14 @@ close(SerialPort) ->
   ok.
   
 send(SerialPort) ->
-  SerialPort ! {send, "Hello World\r\n"},
+  SerialPort ! {send, <<16#68040468FFCE01018F9C93F516:13/unit:8>>},
   ok.
 
 listen() ->
   receive
     % Receive data from the serial port on the caller's PID.
     {data, Bytes} ->
-      io:format("~s", [Bytes]),
+      io:format("~s", [[io_lib:format("~2.16.0B",[X]) || <<X:8>> <= Bytes ]]),
       listen()
   after
     % Stop listening after 5 seconds of inactivity.
