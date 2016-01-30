9b62bb61036adf4ba869f1322b824a113e9f5f1d
Development RPi <pi@raspberrypi>
2016-01-30 16:00:30 +0000

    Disable hardware control
    Fixes RTS peak bug

--- c_src/serial.c
+++ c_src/serial.c
@@ -182,7 +182,7 @@ void set_raw_tty_mode(int fd)
   /* roland */
   ttymodes.c_cflag |= CLOCAL;
 
-
+  ttymodes.c_cflag &= ~CRTSCTS;    /* disable hardware flow control */
 
   /* Apply changes */
 
@@ -222,8 +222,6 @@ void set_tty_speed(int fd, speed_t new_ispeed, speed_t new_ospeed)
       perror("cfsetospeed");
       exit(1);
     }
-
-  ttymodes.c_cflag |= CRTSCTS;     /* enable RTS/CTS flow control */
 
   /* Apply hanges */
 
